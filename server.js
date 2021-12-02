4b274c900766c8b935694d280339318651e3e736
Gwen McArdle <mcardleg@tcd.ie>
2021-12-02 14:27:58 +0000

    queries succesful, not all movies are being inserted

--- server.js
+++ server.js
@@ -33,10 +33,6 @@ const create_tables = () => {
     });
 }
 
-const get_data = () => {
-    
-}
-
 const populate_tables = () => {
     console.log("Importing movies into DynamoDB. Please wait.");
 
@@ -65,9 +61,7 @@ const create_db = async() => {
     const p = new Promise(resolve => {
         create_tables();
         resolve();
-    })
-    await p;
-    populate_tables();
+    }).then(populate_tables());
 }
 
 const delete_db = () => {
@@ -84,6 +78,36 @@ const delete_db = () => {
     });
 }
 
-create_db();
+const query_db = (year, title_searched) => {
+    console.log("Querying for movies from " + year + " with titles starting with " + title_searched);
 
-//delete_db();
+    var params = {
+        TableName : "Movies",
+        ProjectionExpression:"#yr, title, info.genres, info.actors[0]",
+        KeyConditionExpression: "#yr = :yyyy and begins_with (title, :title_searched)",
+        ExpressionAttributeNames:{
+            "#yr": "year"
+        },
+        ExpressionAttributeValues: {
+            ":yyyy": year,
+            ":title_searched": title_searched
+        }
+    };
+
+    docClient.query(params, function(err, data) {
+        if (err) {
+            console.log("Unable to query. Error:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Query succeeded.");
+            data.Items.forEach(function(item) {
+                console.log(" -", item.year + ": " + item.title
+                + " ... " + item.info.genres
+                + " ... " + item.info.actors[0]);
+            });
+        }
+    });
+}
+
+// create_db();
+// delete_db();
+query_db(2013, "A Field");
